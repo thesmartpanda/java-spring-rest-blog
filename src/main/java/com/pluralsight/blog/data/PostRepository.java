aedb40e1cfd5c5cf3558914fdfda014fb7e6dcc2
Mathieu Wautier <mathieu.wautier@gmail.com>
2021-12-26 22:13:50 +0100

    created post repository

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,6 +3,7 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -10,20 +11,9 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-public class PostRepository {
-    public List<Post> findAll() {
-        return new ArrayList<>();
-    }
+public interface PostRepository extends JpaRepository<Post, Long> {
 
-    public List<Post> saveAll(List<Post> posts) {
-        return new ArrayList<>();
-    }
+    @RestResource(rel="contains-title", path="containsTitle")
+    List<Post> findByTitleContaining(String title);
 
-    public Post save(Post post) {
-        return null;
-    }
-
-    public Optional<Post> findById(Long id) {
-        return null;
-    }
 }
